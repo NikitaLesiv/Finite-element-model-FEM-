adb2614b07e51039a5fb4106686ed76c745309c7
Никита Лесив <nikita.lesiv@gmail.com>
2019-01-18 00:20:20 +0300

    Добавьте файлы проекта.

---
+++ FEM/WF.cpp
@@ -0,0 +1,13 @@
+#include "WF.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	FEM::WF WF;
+	Application::Run(%WF);
+}
